c80d89acfff561d3e1d0e1769ea0a567405cee44
Bhanu sai <113322528+bhanusaionly55@users.noreply.github.com>
2026-04-03 21:46:42 +0530

    Comment out database engine and session creation
    Comment out engine and SessionLocal initialization.

--- database.py
+++ database.py
@@ -3,6 +3,6 @@ from sqlalchemy import create_engine
 
 db_url = "postgresql://postgres:Bhanu%40555@localhost:5432/fastapi"
 
-engine = create_engine(db_url)
+#engine = create_engine(db_url)
 
-SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+#SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
